801ed1b1eef5ef7a243240e40793a980c6fc2958
sindunata <sindunata@balitower.co.id>
2024-09-19 17:57:43 +0700

    first

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Modal } from "@/components/ui/modal"
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
+        Children
+      </Modal>
+    </div>
+  )
+}
+  
+export default SetupPage
